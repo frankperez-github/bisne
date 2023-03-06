45d9199b7327a5b19b7c0535651689be7af0b83d
Frank Pérez <fp848584@gmail.com>
2023-03-05 20:17:13 -0500

    promo added.

--- src/pages/Announcement.jsx
+++ src/pages/Announcement.jsx
@@ -9,17 +9,15 @@ function Announcement()
     const {announcements} = useContext(AnnouncementsContext)
     const router = useRouter()
     const {id} = router.query
-    const[Announcement, setAnnouncement] = useState(announcements[id-1]);
     const[descrArray, setDescriptArray] = useState([])
     useEffect(()=>(
-        setAnnouncement(announcements[id-1]),
         setDescriptArray(announcements[id-1].description.split('\n'))
     ),[])
         
-        var princImage = Announcement.images == undefined ? `/${Announcement.category}.png` : Announcement.images[0]
+        var princImage = announcements[id-1].images == undefined ? `/${announcements[id-1].category}.png` : announcements[id-1].images[0]
         
     return(
-        Announcement !== undefined &&
+        announcements[id-1] !== undefined &&
         
 
             <div className="Announcement">
@@ -31,7 +29,7 @@ function Announcement()
                     <Image className="image" src={princImage} alt="" fill/>
                 </div>
 
-                <h2 className="announcInfoLine">{Announcement.name}</h2>
+                <h2 className="announcInfoLine">{announcements[id-1].name}</h2>
                     { descrArray && descrArray.map(line =>(
                         <p key={line} className="announcInfoLine">{line}</p>
                     ))}
@@ -39,10 +37,10 @@ function Announcement()
                 <br/>
                 <div className="contact">
                     <p className="announcInfoLine">Contacto: </p>
-                    <a className="announcInfoLine" href={`tel:${Announcement.phone}`}>{Announcement.phone}</a>
+                    <a className="announcInfoLine" href={`tel:${announcements[id-1].phone}`}>{announcements[id-1].phone}</a>
                 </div>
 
-                <p className="price announcInfoLine">${Announcement.price} {Announcement.currency}</p>
+                <p className="price announcInfoLine">${announcements[id-1].price} {announcements[id-1].currency}</p>
             </div>
         </div>
     );
