e31eb398f8e78e775bd9a620402a728cec69c56c
Frank Pérez <fp848584@gmail.com>
2023-03-24 10:28:24 -0400

    analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,14 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-E7P5CSE90D"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-E7P5CSE90D"></script>
       <script dangerouslySetInnerHTML={{
                             __html:`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
 
-        gtag('config', 'G-E7P5CSE90D');`,}} /> */}
+        gtag('config', 'G-E7P5CSE90D');`,}} />
       <body>
         <Main />
         <NextScript />
