206586805f76a2dd84db7383e03571072feec782
Frank Pérez <fp848584@gmail.com>
2023-03-24 10:53:04 -0400

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,18 @@ import { useRouter } from 'next/router';
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+        const handleRouteChange = (url) => {
+          gtag.pageview(url);
+        };
+     
+        router.events.on("routeChangeComplete", handleRouteChange);
+     
+        return () => {
+          router.events.off("routeChangeComplete", handleRouteChange);
+        };
+      }, [router.events]);
   return (
     <>
     <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
