26708ef88e6b5a81405f24f8fb18edd9a26ca3c2
Frank Pérez <fp848584@gmail.com>
2023-03-24 00:50:36 -0400

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,22 @@
 import '@/styles/globals.css'
 import { AnnouncementsContextProvider } from '@/context/AnnouncementsContext'
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+        window.gtag("config", "G-E7P5CSE90D", {
+            page_path: url,
+        });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+    };
+}, [router.events]);
+
   return (
   <AnnouncementsContextProvider>
     <Component {...pageProps} />
