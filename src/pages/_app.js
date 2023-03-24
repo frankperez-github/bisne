90b67a943392aa27e6013ed0f65434b111c2b349
Frank Pérez <fp848584@gmail.com>
2023-03-24 10:51:19 -0400

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,24 +2,30 @@ import '@/styles/globals.css'
 import { AnnouncementsContextProvider } from '@/context/AnnouncementsContext'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-        window.gtag("config", "G-E7P5CSE90D", {
-            page_path: url,
-        });
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-        router.events.off("routeChangeComplete", handleRouteChange);
-    };
-}, [router.events]);
-
   return (
-  <AnnouncementsContextProvider>
-    <Component {...pageProps} />
-  </AnnouncementsContextProvider>
+    <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E7P5CSE90D', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+
+    <AnnouncementsContextProvider>
+      <Component {...pageProps} />
+    </AnnouncementsContextProvider>
+  </>
   );
 }
