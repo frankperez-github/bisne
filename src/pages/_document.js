aa5c9eeb07bb7657c44cbc89d036a5a3c9fa99c9
Frank Pérez <fp848584@gmail.com>
2023-03-24 23:37:29 -0400

    adsense

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2342183452345155"
+     crossorigin="anonymous"></script>
       <Head />
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-E7P5CSE90D"></script>
       <script dangerouslySetInnerHTML={{
